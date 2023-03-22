e782bd49349b7854ed57a9f5bbcf0473ef673a6a
MitchVerweerden <m.verweerden@student.avans.nl>
2023-03-22 10:06:25 +0100

    Initial Commit

---
+++ IOS-Digivice/IOS_DigiviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_DigiviceApp.swift
+//  IOS-Digivice
+//
+//  Created by Mitchell Verweerden on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS_DigiviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
