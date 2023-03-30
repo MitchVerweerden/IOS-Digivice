67431308887bcf374d42f00c6fa389e780a24345
Mitchell Verweerden <m.verweerden@student.avans.nl>
2023-03-30 10:00:17 +0200

    SplashView fix

---
+++ IOS-Digivice/SplashView.swift
@@ -0,0 +1,39 @@
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        NavigationView {
+            NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true)) {
+                ZStack{
+                    Image("background2").resizable()
+                        .ignoresSafeArea(.all)
+                        .scaledToFill()
+                    VStack {
+                        Image("Digimon-Logo")
+                        Text("Tap anywhere to continue")
+                            .font(.custom("PixelDigivolve", size: 20))
+                            .foregroundColor(.orange)
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+    
+    init(){
+        for familyName in UIFont.familyNames{
+            print(familyName)
+            
+            for fontName in UIFont.fontNames(forFamilyName: familyName){
+                print("-- \(fontName) ")
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
